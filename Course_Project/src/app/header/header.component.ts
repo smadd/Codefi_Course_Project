6872eec4687103995ef1e323d0d8db149e1c10b9
Stephanie <49798581+smadd@users.noreply.github.com>
2021-05-01 16:39:28 -0500

    Adding Navigation with Event Binding and ngIf
    Added $event to app header in app component
    Added ngIf loadedFeature for recipes and non-recipes
    Added loadedFeature to onNavigate
    Added click event to header component
    Added EventEmitter and Output to header component
    Moved ngFor to recipe-list component

--- Course_Project/src/app/header/header.component.ts
+++ Course_Project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component ({
   selector: 'app-header',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent {
 
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+      this.featureSelected.emit(feature);
+  }
 }
 
 
