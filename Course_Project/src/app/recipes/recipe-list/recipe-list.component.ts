79732ef4cc9ae888b0f621438f7298982673c56b
Stephanie <49798581+smadd@users.noreply.github.com>
2021-05-12 23:04:44 -0500

    Using a Service for Cross-Component Communication
    Cross-Component Communication, removing EventEmitter and Output that are replaced with the recipe service to simplify code. Page works same as before, just with cleaner code.

--- Course_Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course_Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 import { Recipe } from '../recipe.model';
@@ -10,7 +10,7 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 
   recipes: Recipe[];
 
@@ -22,7 +22,5 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-}
+
 }
--- Course_Project/src/app/recipes/recipe.service.ts
+++ Course_Project/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe ('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/12/04/02/26/vegetables-1880980_1280.jpg'),
@@ -12,3 +15,5 @@ export class RecipeService {
   }
 
 }
+
+
