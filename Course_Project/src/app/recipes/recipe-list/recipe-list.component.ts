caa9e3f311a1802cfe5cf8c86903fa2affafeb68
Stephanie <49798581+smadd@users.noreply.github.com>
2021-05-12 22:04:38 -0500

    Setting up Services
    Setting up the Services: Create file for recipe.service.ts and shopping-list.service.ts
    
    Managing Recipes in a Recipe Service: taking recipe information from recipe-list.component.ts and moving to recipe.service.ts

--- Course_Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course_Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,15 +12,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe ('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/12/04/02/26/vegetables-1880980_1280.jpg'),
-    new Recipe ('Another Test Recipe', 'This is simply a test', 'https://pixnio.com/free-images/2020/07/23/2020-07-23-12-09-27-800x1200.jpg')
-  ];
-  constructor() { }
+  recipes: Recipe[];
 
-  ngOnInit(): void {
+  constructor(private recipeService: RecipeService) {
+
+   }
+
+  ngOnInit()  {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
-}}
+}
+}
---
+++ Course_Project/src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe ('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/12/04/02/26/vegetables-1880980_1280.jpg'),
+    new Recipe ('Another Test Recipe', 'This is simply a test', 'https://pixnio.com/free-images/2020/07/23/2020-07-23-12-09-27-800x1200.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+}
