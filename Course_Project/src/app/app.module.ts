6f63fbe8a52d4b380fd9826a5f3d99b3f827b1cf
Stephanie <49798581+smadd@users.noreply.github.com>
2021-05-22 18:32:06 -0500

    Routing updates
    Planning structure, setting up routes, adding navigation to recipes and shopping-list

--- Course_Project/src/app/app.module.ts
+++ Course_Project/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -30,6 +32,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
