f3bf82f426d66c9a4327f17ebfd931861009b393
Stephanie <49798581+smadd@users.noreply.github.com>
2021-06-28 10:24:38 -0500

    uncomment initForm
     uncommented this.initForm(); which made ingredients and X button show

--- Course_Project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Course_Project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -20,7 +20,7 @@ export class RecipeEditComponent implements OnInit {
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
-        //this.initForm();
+        this.initForm();
         // console.log(this.editMode);
 
       }
@@ -36,6 +36,8 @@ export class RecipeEditComponent implements OnInit {
     let recipeImagePath = '';
     let recipeDescription = '';
     let recipeIngredients = new FormArray ([]);
+
+
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
@@ -59,13 +61,15 @@ export class RecipeEditComponent implements OnInit {
       'imagePath': new FormControl(recipeImagePath),
       'description': new FormControl(recipeDescription),
       'ingredients': recipeIngredients
+
         });
 
   }
 
-
   get controls() { // a getter!
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
+
+
 
 }
