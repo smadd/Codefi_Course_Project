57b9c6840e987fe0b437a1953d2b86ea4ecbe47c
Stephanie <49798581+smadd@users.noreply.github.com>
2021-06-30 23:45:12 -0500

    HTTP
    New branch--HTTP Requests
    Setting up the DataStorage service in the shared folder

--- Course_Project/src/app/app.module.ts
+++ Course_Project/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -38,6 +38,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
