0ba999f02248520deb835ae2b222be5d251a1e79
Stephanie <49798581+smadd@users.noreply.github.com>
2021-07-18 17:45:25 -0500

    Resolving Data Before Loading
    using Resolver/creating recipes-resolver.service.ts
    some tweaks from Lee to see if will help Fetch from Firebase (getting rid of providers: recipeService)
    RecipeResolverService in app-routing module
    subscribing to fetchRecipes in header
    map and tap from rxjs/operators

--- Course_Project/src/app/recipes/recipe.service.ts
+++ Course_Project/src/app/recipes/recipe.service.ts
@@ -9,22 +9,22 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 
 @Injectable()
-
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
 
-  private recipes: Recipe[] = [
-    new Recipe ('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/12/04/02/26/vegetables-1880980_1280.jpg', [
-      new Ingredient('Veggie Burger', 1),
-      new Ingredient('French Fries', 20)
-    ]),
-    new Recipe ('Another Test Recipe', 'This is simply a test', 'https://pixnio.com/free-images/2020/07/23/2020-07-23-12-09-27-800x1200.jpg',
-    [
-      new Ingredient('Strawberries', 7),
-      new Ingredient('Kiwis', 2)
-    ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe ('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/12/04/02/26/vegetables-1880980_1280.jpg', [
+  //     new Ingredient('Veggie Burger', 1),
+  //     new Ingredient('French Fries', 20)
+  //   ]),
+  //   new Recipe ('Another Test Recipe', 'This is simply a test', 'https://pixnio.com/free-images/2020/07/23/2020-07-23-12-09-27-800x1200.jpg',
+  //   [
+  //     new Ingredient('Strawberries', 7),
+  //     new Ingredient('Kiwis', 2)
+  //   ])
+  // ];
+private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
--- Course_Project/src/app/shared/data-storage.service.ts
+++ Course_Project/src/app/shared/data-storage.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+
 
 
 
@@ -21,11 +23,23 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
-    .get<Recipe[]>('https://recipe-shopping-list-project-default-rtdb.firebaseio.com/recipes.json')
-    .subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
-    });
-  }
+    return this.http
+    .get<Recipe[]>(
+      'https://recipe-shopping-list-project-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(
+      map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    )
 
+
+}
 }
